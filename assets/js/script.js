ee0e705139b8c7e4c81a2273eb92cccf17b023bb
rodriguestiago <tiagogameiro15@gmail.com>
2020-02-10 16:12:09 +0100

    films

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/* $(document).ready(function(){
+    $('.main-slider').slick({
+
+    });
+  }); */
+
+  /* $('.main-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 1, */
+/*   //  adaptiveHeight: true, */
+ /*    autoplay: true,
+    autoplaySpeed: 6000
+  }); */
