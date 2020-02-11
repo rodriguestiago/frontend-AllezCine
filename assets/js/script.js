0de1d4c29d2785aa805c314404a9d76fd93b7ac7
rodriguestiago <tiagogameiro15@gmail.com>
2020-02-11 11:52:17 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,8 @@
-/* $(document).ready(function(){
-    $('.main-slider').slick({
-
-    });
-  }); */
-
-  /* $('.main-slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 400,
-    slidesToShow: 1, */
-/*   //  adaptiveHeight: true, */
- /*    autoplay: true,
-    autoplaySpeed: 6000
-  }); */
+$('.main-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 400,
+  slidesToShow: 1,
+//  adaptiveHeight: true,
+ 
+});
