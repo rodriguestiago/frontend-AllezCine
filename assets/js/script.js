875761470d5e60d870437bf1a193616f409b4911
Corentin <corentin.colart@gmail.com>
2020-02-12 10:43:03 +0100

    Login and register done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 function login() {
+    $('#modlogin').modal('toggle')
+    }
 
-    $('#modlogin').modal('toggle');
-    
+    document.getElementById('login').addEventListener('click', login)
+
+
+function register() {
+    $('#modregister').modal('toggle')
     }
     
-    document.getElementById('login').addEventListener('click', login)
+    document.getElementById('register').addEventListener('click', register)
